3fdccd1820ce5bd459016230a57c6606277fc8e3
Nikita Molodkin <nikitamolodkin@Nikitas-MacBook-Pro.local>
2025-05-13 15:39:25 +0300

    Initial commit

---
+++ src/main/java/org/example/jwtproject/JwTprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwTprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jwtproject/JwTprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
